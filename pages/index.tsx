84a8c608b05a950f53452b46a287d8d031c0fc5a
Markus Nyman <nymak@tuta.io>
2021-11-03 21:45:41 +0200

    Remove react-chrono and add logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,10 @@
 import type {GetServerSideProps, NextPage} from 'next'
-import { Chrono } from 'react-chrono'
-import {Event} from "../types";
-import Link from "next/link";
+import TF150Logo from "../components/TF150Logo";
 
-interface Props {
-    events: Event[]
-}
-
-const Home: NextPage<Props> = ({ events }) => {
-    const items = events.map(event => ({
-        title: (new Date(event.date)).toLocaleString()
-    }))
+const Home: NextPage = () => {
   return (
-    <div>
-        <Chrono
-            items={items}
-            mode='VERTICAL_ALTERNATING'
-            hideControls
-            theme={{
-                primary: 'red',
-                secondary: 'dark-red',
-                titleColor: 'red'
-            }}
-            cardWidth={500}
-            enableOutline={true}
-        >
-            {events.map(event => (
-                <div key={event.id}>
-                    <div>
-                        <h2>{event.title}</h2>
-                    </div>
-                    <p>{event.description}</p>
-                    
-                    <Link href={event.slug}><a>Read More</a></Link>
-                </div>
-            ))}
-        </Chrono>
+    <div className={"logo"}>
+        <TF150Logo />
     </div>
   )
 }
