a016359c2af5e912d63830e84605b7793596bbf1
Markus Nyman <nymak@tuta.io>
2021-11-07 16:47:18 +0200

    Split css files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/index.css'
+import '../styles/slug.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
