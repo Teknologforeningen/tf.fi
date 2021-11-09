a45eb85ee6db8a0d91a52bb68abd56738e77afe7
Markus Nyman <nymak@tuta.io>
2021-11-09 07:26:28 +0200

    More VerticalLines and add scroll

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -6,6 +6,8 @@ import { Event } from '../types'
 import {useState} from "react";
 import {groupEventsByDate} from "../utils";
 
+const DAYS_IN_YEAR = 365
+
 // receives events as props
 interface Props {
     events: Event[]
@@ -17,18 +19,21 @@ const Timeline: NextPage<Props> = ({ events }) => {
 
     const grouped = groupEventsByDate(events)
 
+    const verticalLinesPerInterval = (DAYS_IN_YEAR - events.length) / events.length
+
+    // For some reason, there are less VerticalLines rendered before the first EventLine
     return (
     <Row center className={'timeline'}>
             {Object.values(grouped).map((e, i) => {
                 return (
                     <Row key={i}>
-                        {[...Array(8)].map((i) => (<VerticalLine key={i}/>))}
+                        {[...Array(verticalLinesPerInterval)].map((i) => (<VerticalLine key={i}/>))}
                         <EventLine events={e} eventToShow={eventToShow} setEventToShow={setEventToShow}/>
                     </Row>
                 )
             })}
             <Row>
-            {[...Array(8)].map((i) => (<VerticalLine key={i}/>))}
+            {[...Array(verticalLinesPerInterval)].map((i) => (<VerticalLine key={i}/>))}
             </Row>
     </Row>
 )
