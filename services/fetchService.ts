3ae1969194d271cf54e043a98936a98c21bf68c4
Markus Nyman <nymak@tuta.io>
2022-01-05 21:03:45 +0200

    Fix fetch path

--- services/fetchService.ts
+++ services/fetchService.ts
@@ -1,4 +1,4 @@
 export async function fetchMultiple<T>(path: string): Promise<T[]> {
-  const res = await fetch(`${process.env.BACKEND_URL}/${path}`)
+  const res = await fetch(`${process.env.BACKEND_URL}${path}`)
   return res.json()
 }
