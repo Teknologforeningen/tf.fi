0744012e575b8c743a43ab6fed7fbfa9de8eb390
Markus Nyman <nymak@tuta.io>
2022-01-06 03:23:37 +0200

    Fix event box styling

--- components/eventline/EventBox.tsx
+++ components/eventline/EventBox.tsx
@@ -10,39 +10,46 @@ interface Props {
 }
 
 /** Box where a short description and title of an event can be displayed */
-const EventBox: NextPage<Props> = ({ event, onlyTitle, ...props }) =>
-  onlyTitle ? (
-    <Column
-      className={`event-box-small ${event.type}-title top-margin`}
-      {...props}
-    >
-      <Link href={event.slug} passHref>
-        <a className={`event-box-sub-title`}>
-          {event.type === 'event' ? 'EVENEMANG' : 'BLOGINLÄGG'}
-        </a>
-      </Link>
-      <Link href={event.slug} passHref>
-        <a className={`event-box-title`}>{event.title}</a>
-      </Link>
-    </Column>
-  ) : (
-    <div className="event-box" {...props}>
-      <Column>
-        <Column className={`event-box-small ${event.type}-title`} {...props}>
-          <p className={`event-box-sub-title`}>
+const EventBox: NextPage<Props> = ({ event, onlyTitle, ...props }) => {
+  if (onlyTitle) {
+    return (
+      <Column
+        className={`event-box-small ${event.type}-title-container top-margin`}
+        {...props}
+      >
+        <Link href={event.slug} passHref>
+          <a className={`event-box-sub-title`}>
             {event.type === 'event' ? 'EVENEMANG' : 'BLOGINLÄGG'}
-          </p>
-          <p className={`event-box-title`}>{event.title}</p>
-        </Column>
-        <div className="event-box-horizontal-line" />
-        <p className="event-box-text">{event.description}</p>
-        <div className="event-box-special">
-          <Link href={event.slug} passHref>
-            <a className="event-box-link">LÄS MERA</a>
-          </Link>
-        </div>
+          </a>
+        </Link>
+        <Link href={event.slug} passHref>
+          <a className={`event-box-title`}>{event.title}</a>
+        </Link>
       </Column>
-    </div>
-  )
-
+    )
+  } else {
+    return (
+      <div className="event-box" {...props}>
+        <Column className={'event-box-container'}>
+          <Column
+            className={`event-box-small ${event.type}-title-container`}
+            {...props}
+          >
+            <p className={`event-box-sub-title`}>
+              {event.type === 'event' ? 'EVENEMANG' : 'BLOGINLÄGG'}
+            </p>
+            <p className={`event-box-title`}>{event.title}</p>
+          </Column>
+          <div className="event-box-horizontal-line" />
+          <p className="event-box-text">{event.description}</p>
+          <div className="event-box-special">
+            <Link href={event.slug} passHref>
+              <a className="event-box-link">LÄS MERA</a>
+            </Link>
+          </div>
+        </Column>
+      </div>
+    )
+  }
+}
 export default EventBox
