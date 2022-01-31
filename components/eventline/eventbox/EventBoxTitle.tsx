e83b3682826a8a3dbc6d37af2ac16adc16f7f2f6
Markus Nyman <nymak@tuta.io>
2022-01-31 10:43:01 +0200

    Fix EventBox title overflow

--- components/eventline/eventbox/EventBoxTitle.tsx
+++ components/eventline/eventbox/EventBoxTitle.tsx
@@ -2,27 +2,37 @@ import { NextPage } from 'next'
 import Column from '../../Column'
 import { EventBoxProps } from './EventBox'
 import Link from 'next/link'
+import { Textfit } from 'react-textfit'
+import { useEffect } from 'react'
 
 const EventBoxTitle: NextPage<EventBoxProps> = ({
   event,
   topMargin = false,
   ...props
-}) => (
-  <Column
-    className={`event-box-small event-box-small-${event.type} ${
-      topMargin ? 'top-margin' : ''
-    }`}
-    {...props}
-  >
-    <Link key={event.id} href={`/${event.slug}`}>
-      <a>
-        <p className={`event-box-sub-title`}>
-          {event.type === 'event' ? 'EVENEMANG' : 'BLOGGINLÄGG'}
-        </p>
-        <p className={`event-box-title`}>{event.title}</p>
-      </a>
-    </Link>
-  </Column>
-)
+}) => {
+  useEffect(() => {
+    window.dispatchEvent(new CustomEvent('resize'))
+  }, [])
+
+  return (
+    <Column
+      className={`event-box-small event-box-small-${event.type} ${
+        topMargin ? 'top-margin' : ''
+      }`}
+      {...props}
+    >
+      <Link key={event.id} href={`/${event.slug}`}>
+        <a>
+          <p className={`event-box-sub-title`}>
+            {event.type === 'event' ? 'EVENEMANG' : 'BLOGGINLÄGG'}
+          </p>
+          <Textfit mode="multi" min={10} max={20} className={`event-box-title`}>
+            {event.title}
+          </Textfit>
+        </a>
+      </Link>
+    </Column>
+  )
+}
 
 export default EventBoxTitle
