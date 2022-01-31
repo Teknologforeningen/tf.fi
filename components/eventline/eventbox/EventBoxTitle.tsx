e0d4af306c5891e9fc674311fca0c8be6917d331
Markus Nyman <nymak@tuta.io>
2022-01-31 10:43:01 +0200

    Fix EventBox title overflow

--- components/eventline/eventbox/EventBoxTitle.tsx
+++ components/eventline/eventbox/EventBoxTitle.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next'
 import Column from '../../Column'
 import { EventBoxProps } from './EventBox'
 import Link from 'next/link'
+import { Textfit } from 'react-textfit'
 
 const EventBoxTitle: NextPage<EventBoxProps> = ({
   event,
@@ -19,7 +20,9 @@ const EventBoxTitle: NextPage<EventBoxProps> = ({
         <p className={`event-box-sub-title`}>
           {event.type === 'event' ? 'EVENEMANG' : 'BLOGGINLÄGG'}
         </p>
-        <p className={`event-box-title`}>{event.title}</p>
+        <Textfit mode="multi" max={20} className={`event-box-title`}>
+          {event.title}
+        </Textfit>
       </a>
     </Link>
   </Column>
