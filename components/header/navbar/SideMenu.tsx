1ad2058c2e7fd82e391b2af40a6f08c3ec7f40f7
Markus Nyman <nymak@tuta.io>
2022-08-17 22:04:58 +0300

    SideMenu to Tailwind

--- components/header/navbar/SideMenu.tsx
+++ components/header/navbar/SideMenu.tsx
@@ -9,7 +9,7 @@ interface Props {
 const SideMenu: NextPage<Props> = (props) => {
   return (
     <div
-      className={'side-menu'}
+      className="fixed top-0 left-0 w-[90vw] min-h-screen z-10 bg-darkblue shadow-[0_0_200px_rgba(0,0,0,0.9)_inset] py-16 px-8 duration-[0.4s] overflow-x-hidden descendant:mb-2"
       style={{
         transform: props.open ? 'translateX(0)' : 'translateX(-100%)',
       }}
