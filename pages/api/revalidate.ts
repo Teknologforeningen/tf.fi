9813ffc1cdf022c99d01d101f4c8a3c7e9390ae6
Markus Nyman <markus@nyman.dev>
2022-10-10 02:29:32 +0300

    Allow revalidation by setting query param

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,23 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+async function revalidate(res: NextApiResponse, path: string) {
+  await res.revalidate(path)
+  console.log('Revalidated path', path)
+  res.json({ revalidated: true })
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const bearer = req.headers.authorization?.slice(7)
-  if (bearer !== process.env.API_KEY) {
-    res.status(401).json({ error: 'Invalid token' })
-  }
+  if (bearer !== process.env.API_KEY)
+    return res.status(401).json({ error: 'Invalid token' })
 
   try {
+    const path = req.query.path
+    if (path != null) return await revalidate(res, path as string)
+
     const model = req.body.model
     const slug = req.body.entry.slug
     const prefix = model === 'about-page' ? '/about/' : '/'
-    if (slug == null) res.status(400).json({ error: 'Missing slug' })
+    if (slug == null) return res.status(400).json({ error: 'Missing slug' })
 
-    await res.revalidate(`${prefix}${slug}`)
-    console.log(`Revalidated ${prefix}${slug}`)
-    res.json({ revalidated: true })
+    return await revalidate(res, `${prefix}${slug}`)
   } catch (err) {
     console.log(err)
     return res.status(500).send('Error revalidating')
