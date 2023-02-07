3a4ef30bad3c554d35ef1a83abe83ce81baf5d42
lindejoh <johan.lindell@aalto.fi>
2023-02-07 15:06:54 +0200

    minor fixes to namo kallelse

--- components/namokallese/Namokallelse.tsx
+++ components/namokallese/Namokallelse.tsx
@@ -14,24 +14,21 @@ type NamoKallelseProps = {
 const Namokallelses = ({ namokallelses = [] }: NamoKallelseProps) => {
   const [show, setShow] = useState(true)
   return (
-    <div className="sticky left-0 bottom-0 z-10 w-full py-4 px-6 text-gray-700">
-      <button
-        className="container mx-auto"
-        onClick={() => setShow(false)}
-        disabled={!show}
-      >
-        {namokallelses.map((namo) => (
-          <div
-            key={namo.id}
-            className={`my-4 rounded-lg bg-gray-200 p-4 shadow-md transition-all duration-500 ${
-              !show && 'transform opacity-0'
-            }`}
-          >
-            <p className="font-medium">{namo.title}</p>
-            <p className="text-sm text-gray-600">{namo.content}</p>
-          </div>
-        ))}
-      </button>
+    <div
+      className={`sticky bottom-0 z-10 mx-auto w-full self-center p-3 text-gray-700 ${
+        !show && 'pointer-events-none transform opacity-0'
+      }`}
+      onClick={() => setShow(false)}
+    >
+      {namokallelses.map((namo) => (
+        <div
+          key={namo.id}
+          className="my-4 rounded-lg bg-gray-200 p-4 shadow-md transition-all duration-500"
+        >
+          <p className="select-none font-medium">{namo.title}</p>
+          <p className="select-none text-sm text-gray-600">{namo.content}</p>
+        </div>
+      ))}
     </div>
   )
 }
