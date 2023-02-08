d2fb1d283bdcecdeb7b9453c404f673bf60e83c7
Johan Lindell <60311153+johan-lindell@users.noreply.github.com>
2023-02-08 08:44:18 +0200

    feature: namo kallelse popup (#17)
    feature: namo kallelse popup

---
+++ components/namokallese/Namokallelse.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+export type Namo = {
+  id: number
+  title: string
+  content: string
+  date: string
+  locale: string
+}
+type NamoKallelseProps = {
+  namokallelses: Namo[]
+}
+
+const Namokallelses = ({ namokallelses = [] }: NamoKallelseProps) => {
+  const [show, setShow] = useState(true)
+  return (
+    <div
+      className={`sticky bottom-0 z-10 mx-auto w-full self-center p-3 text-gray-700 transition-all duration-500 ${
+        !show && 'pointer-events-none transform opacity-0'
+      }`}
+      onClick={() => setShow(false)}
+    >
+      {namokallelses.map((namo) => (
+        <div
+          key={namo.id}
+          className="my-4 rounded-lg bg-gray-200 p-4 shadow-md"
+        >
+          <p className="select-none font-medium">{namo.title}</p>
+          <p className="select-none text-sm text-gray-600">{namo.content}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Namokallelses
