ea97176d7937dd497284e6c406b40fa98e055121
Markus Nyman <markus@nyman.dev>
2023-06-20 23:15:22 +0300

    Remove AuthContext

--- components/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-export const AuthContext = dynamic(
-  () =>
-    import('@hipsquare/react-strapi-keycloak-auth-context').then(
-      (mod) => mod.WithAuthContext
-    ),
-  { ssr: false }
-)
