af1a6f5752494e7d5cc65d344274dc8a32732b74
Markus Nyman <markus@nyman.dev>
2023-08-12 18:54:49 +0300

    Do not error when footer or nationlogos is missing

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,8 +21,8 @@ export interface HomePage {
       data: BannerImage[]
     }
   }
-  footer: {
-    nationlogos: NationLogo[]
+  footer?: {
+    nationlogos?: NationLogo[]
   }
 }
 
@@ -82,12 +82,11 @@ export const getStaticProps: GetStaticProps = async () => {
   const events = await fetchEvents(1)
   const homepage = await fetchHomepage()
   const navbarLinks = await fetchNavbar()
-  const logos = homepage.footer.nationlogos
   return {
     props: {
       navbarLinks,
       events: events.data,
-      logos,
+      logos: homepage?.footer?.nationlogos ?? [],
       bannerImages: homepage.banner?.bannerImages?.data ?? [],
     },
   }
