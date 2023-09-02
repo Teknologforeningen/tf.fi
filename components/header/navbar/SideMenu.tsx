ab2bcf7f5ef7b3e0729241755cf378df55e8d601
Jonas Bäck <jonas.c.back@gmail.com>
2023-09-02 21:53:07 +0300

    Remove framer-motion from navbar
    For some reason it caused private pages to return 500
    when refreshing

--- components/header/navbar/SideMenu.tsx
+++ components/header/navbar/SideMenu.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react'
-import { motion } from 'framer-motion'
 
 type SideMenuProps = {
   open: boolean
@@ -7,14 +6,13 @@ type SideMenuProps = {
 }
 
 const SideMenu = ({ open, children }: SideMenuProps) => (
-  <motion.div
-    className="fixed left-0 top-0 z-10 min-h-screen w-full overflow-x-hidden bg-darkgray px-8 py-16 descendant:mb-2 md:hidden"
-    initial={false}
-    animate={{ transform: open ? 'translateX(0)' : 'translateX(-100%)' }}
-    transition={{ duration: 0.4 }}
+  <div
+    className={`${
+      open ? 'translate-x-0' : '-translate-x-full'
+    } duration-400 fixed left-0 top-0 z-10 min-h-screen w-full overflow-x-hidden bg-darkgray px-8 py-16 transition descendant:mb-2 md:hidden`}
   >
     {children}
-  </motion.div>
+  </div>
 )
 
 export default SideMenu
