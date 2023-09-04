6555487b2effbef5f509e19d5a886f5d9b1239ed
Jonas Bäck <jonas.c.back@gmail.com>
2023-09-04 13:15:21 +0300

    Update table of contents styling

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -4,18 +4,23 @@ import { titleToAnchor } from '@utils/helpers'
 
 const TableOfContents = ({ sections }: { sections: Section[] }) => {
   return (
-    <ul>
-      {sections.map((section, i) => (
-        <li key={i}>
-          <Link
-            className="no-underline hover:underline"
-            href={`#${titleToAnchor(section.attributes.title)}`}
-          >
-            {section.attributes.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2>Innehållsförteckning</h2>
+      <table className="table-auto border-separate text-sm">
+        <tbody>
+          {sections.map((section, i) => (
+            <tr key={i}>
+              <Link
+                className="no-underline hover:underline"
+                href={`#${titleToAnchor(section.attributes.title)}`}
+              >
+                {section.attributes.title}
+              </Link>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
 
