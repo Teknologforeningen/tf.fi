3fc7fa024c0c6b34d90093838dce5441a162b719
Markus Nyman <markus@nyman.dev>
2023-09-25 00:10:05 +0300

    Extract common layouts to PageLayout

--- app/medlem/[privatePage]/layout.tsx
+++ app/medlem/[privatePage]/layout.tsx
@@ -1,13 +1,3 @@
-import React from 'react'
+import PageLayout from '@components/PageLayout'
 
-const MedlemPageLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="min-h-screen">
-    <div className="flex flex-grow justify-center">
-      <div className="prose prose-sm mx-4 mb-12 mt-6 flex flex-col sm:mx-8 md:mx-16 md:mt-12">
-        {children}
-      </div>
-    </div>
-  </div>
-)
-
-export default MedlemPageLayout
+export default PageLayout
