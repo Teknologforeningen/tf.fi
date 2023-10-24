d0d4cd9f044603664b1dbaad36081bc81652405b
Jonas Bäck <jonas.c.back@gmail.com>
2023-10-24 14:48:33 +0300

    Fetch calendar events on page load

--- components/calendar/Calendar.tsx
+++ components/calendar/Calendar.tsx
@@ -3,10 +3,7 @@ import Subtitle from '@components/Subtitle'
 import listEvents, { CalendarEvent } from '@lib/google/calendar'
 import InteractiveCalendar from '@components/calendar/InteractiveCalendar'
 
-const Calendar = async () => {
-  const now = new Date()
-  const events = await listEvents(now)
-
+const Calendar = () => {
   const fetchMore = async (date: Date): Promise<CalendarEvent[]> => {
     'use server'
     return listEvents(date)
@@ -17,11 +14,7 @@ const Calendar = async () => {
       <div className="self-center md:self-auto">
         <Subtitle underline={false}>Händelsekalendern</Subtitle>
       </div>
-      <InteractiveCalendar
-        initialEvents={events}
-        initialDate={now}
-        fetchEvents={fetchMore}
-      />
+      <InteractiveCalendar fetchEvents={fetchMore} />
     </div>
   )
 }
