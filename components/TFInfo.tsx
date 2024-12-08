2ea80e21c5c4505e0b232d62e6de3870214d9f6e
Markus Nyman <markus@nyman.dev>
2024-12-08 17:07:29 +0200

    Use prose on TFInfo

--- components/TFInfo.tsx
+++ components/TFInfo.tsx
@@ -3,9 +3,9 @@ import Column from './Column'
 
 const TFInfo = () => {
   return (
-    <Column className="text-black">
-      <h1 className="text-3xl font-bold">Vad är TF?</h1>
-      <p className="max-w-[1200px] py-3">
+    <Column className="prose 2xl:prose-xl">
+      <h2>Vad är TF?</h2>
+      <p className="">
         Vad är TF? Teknologföreningen, TF, är den svenskspråkiga nationen vid
         Aalto-universitetet. Nationen grundades 1872. TF:s ändamål är att
         sammanföra svenskspråkiga studerande vid Aalto-universitetet. I och med
