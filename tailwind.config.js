f4681dbe4a067f352771f34ca4c42c062280cf2c
Markus Nyman <markus@nyman.dev>
2024-12-08 22:23:06 +0200

    Decrease heading weights

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,15 +31,31 @@ module.exports = {
             fontSize: '1rem',
             h1: {
               fontSize: '2.5rem',
+              fontWeight: 600,
+            },
+            'h1 strong': {
+              fontWeight: 700,
             },
             h2: {
               fontSize: '2rem',
+              fontWeight: 500,
+            },
+            'h2 strong': {
+              fontWeight: 600,
             },
             h3: {
               fontSize: '1.5rem',
+              fontWeight: 500,
+            },
+            'h3 strong': {
+              fontWeight: 600,
             },
             h4: {
               fontSize: '1.25rem',
+              fontWeight: 500,
+            },
+            'h4 strong': {
+              fontWeight: 600,
             },
           },
         },
