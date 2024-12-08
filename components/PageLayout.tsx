e3f1caa5c97d4578400b9ff1c55fb29fa671d414
Markus Nyman <markus@nyman.dev>
2024-12-08 22:27:22 +0200

    Fix padding around content pages

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PageLayout = ({ children }: { children: React.ReactNode }) => (
   <div className="min-h-screen">
     <div className="flex flex-grow justify-center">
-      <article className="prose prose-sm xxs:prose-base w-[95vw] 2xl:prose-xl mx-4 mb-12 mt-6 flex flex-col sm:mx-8 md:mx-16 md:mt-12">
+      <article className="prose prose-sm xxs:prose-base w-[95vw] 2xl:prose-xl px-4 py-16 flex flex-col sm:px-8 md:px-16 md:pt-24">
         {children}
       </article>
     </div>
