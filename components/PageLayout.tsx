b4f8d0a8cf911bbcc799a76debd079c8d240d7e2
Markus Nyman <markus@nyman.dev>
2024-12-08 17:07:40 +0200

    Increase font size on content pages

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PageLayout = ({ children }: { children: React.ReactNode }) => (
   <div className="min-h-screen">
     <div className="flex flex-grow justify-center">
-      <div className="prose prose-sm mx-4 mb-12 mt-6 flex flex-col sm:mx-8 md:mx-16 md:mt-12">
+      <div className="prose 2xl:prose-xl mx-4 mb-12 mt-6 flex flex-col sm:mx-8 md:mx-16 md:mt-12">
         {children}
       </div>
     </div>
