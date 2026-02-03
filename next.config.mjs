986a0c2d5ed68f43395d3dfe8938bde9a9ae9cef
Markus Nyman <markus@nyman.dev>
2026-02-03 14:46:35 +0200

    Enable React compiler

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactCompiler: true,
   reactStrictMode: true,
   images: {
     remotePatterns: [
